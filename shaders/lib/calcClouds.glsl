c8db200089070d2447489af9aea449811955bd38
BuildTools <rstikfort@gmail.com>
2017-05-15 15:49:52 +0200

    Changed the look of the 2D cirrus clouds

--- shaders/lib/calcClouds.glsl
+++ shaders/lib/calcClouds.glsl
@@ -12,41 +12,37 @@
 		return x * (pi / 180.0); 
 	}
 
-	float cloudNoise(vec2 coord){
+	float cloudNoise(vec2 coord, vec2 wind){
 		float noise = 1.0;
 
-		coord.y *= 2.0;
-		coord.x *= 0.5;
+		coord -= wind;
 
-		noise = texture2D(noisetex, vec2(0.0, coord.y) * 0.7).x;
-		noise = texture2D(noisetex, vec2(coord.y, coord.x) * 0.7).x * 2.0;
+		noise = texture2D(noisetex, vec2(0.0, (coord.y) * 2.0) * 0.7).x;
+		noise = texture2D(noisetex, vec2(coord.y * 3.0, coord.x * 0.5) * 0.7).x * 2.0;
 		noise /= noise + 1.0;
-		noise -= 0.2;
+		noise -= 0.3;
+		noise = max(noise, 0.0);
 
-		float deg0 = -90.0 - (noise * 45.0 * 0.5);
+		coord += wind;
+
+		float deg0 = 90.0 - noise * 7.0;
 		float rad0 = degreesToRadiance(deg0);
 		mat2 rM0 = mat2(cos(rad0), -sin(rad0), sin(rad0), cos(rad0));
 
-		vec2 rCoord0 = rM0 * coord;
-			 rCoord0.y *= 9.0;
+		vec2 rCoord0 = coord;
+		rCoord0 = rM0 * rCoord0;
+		rCoord0.y *= 3.0;
+		rCoord0.x = 0.0;
 
-		float deg1 = -90.0 - (noise * 45.0 * 0.5);
-		float rad1 = degreesToRadiance(deg1);
-		mat2 rM1 = mat2(cos(rad1), -sin(rad1), sin(rad1), cos(rad1));
-
-		vec2 rCoord1 = rM1 * coord;
-			 rCoord1.y *= 9.0;
-
-		noise += texture2D(noisetex, rCoord0 * 4.0).x * 0.025;
-		noise += texture2D(noisetex, rCoord1 * 16.0).x * 0.0125;
+		noise += texture2D(noisetex, (rCoord0 + wind * 0.5) * 32.0).x * 0.025;
+		noise += texture2D(noisetex, rCoord0 * 32.0).x * 0.025;
 
 		coord.x *= 2.0;
-		noise += (texture2D(noisetex, coord * 4.0).x - 0.1) * 0.05;
+		noise += (texture2D(noisetex, coord * 4.0).x) * 0.05;
 
-		float cl = max(pow(noise, 4.0) - 0.005, 0.0) * 10.0;
+		float cl = max(pow(noise, 4.0) - 0.005, 0.0) * 15.0;
 		cl *= (1.0 - rainStrength * 0.5);
 		cl /= cl + 1.0;
-		cl = cl * cl * (3.0 - 2.0 * cl);
 
 		return cl;
 	}
@@ -62,6 +58,14 @@
 			vec4 fposition = normalize(vec4(fpos,0.0));
 			vec3 tPos = getWorldSpace(fposition).rgb;
 			vec3 wVec = normalize(tPos);
+
+			float height = cloudHeight / wVec.y;
+			vec3 cloudPosition = wVec * height;
+
+			vec2 coord = (cloudPosition.xz + cameraPosition.xz * 2.5) / 200000.0;
+				 coord += wind;
+
+			float totalcloud = cloudNoise(coord, wind);
 			
 			float cosT = clamp(dot(fposition.rgb,upVec),0.0,1.0);
 
@@ -69,27 +73,16 @@
 			float MoonUpCos = clamp(dot(moonVec, upVec) * 0.95 + 0.15, 0.0, 1.0);
 
 			vec3 dayTimeColor = sunlight * sunUpCos;
-			vec3 nightTimeColor = moonlight * MoonUpCos;
+				 dayTimeColor *= 1.0 + subSurfaceScattering(sunVec, fposition.rgb, 5.0) * 10.0 * pow(1.0 - totalcloud, 5.0);
 
-			vec3 cloudCol = (dayTimeColor + nightTimeColor) * 2.0;
+			vec3 nightTimeColor = moonlight * MoonUpCos * 3.0;
+				 nightTimeColor *= 1.0 + subSurfaceScattering(moonVec, fposition.rgb, 5.0) * 10.0 * pow(1.0 - totalcloud, 5.0);
+
+			vec3 cloudCol = (dayTimeColor + nightTimeColor);
 				 cloudCol = mix(cloudCol, ambientlight, rainStrength);
 				 cloudCol *= mix(1.0, 0.5, rainStrength * time[1].y);
 
-			float totalcloud = 	0.0f;
-			float height = 		0.0f;
-
-			vec3 cloudPosition = vec3(0.0);
-				
-			height = cloudHeight / wVec.y;
-
-			cloudPosition = wVec * height;
-
-			vec2 coord = (cloudPosition.xz + cameraPosition.xz * 2.5) / 200000.0;
-				 coord += wind;
-
-			totalcloud = cloudNoise(coord);
-
-			return mix(color, cloudCol ,totalcloud * cosT);
+			return mix(color, cloudCol, totalcloud * cosT);
 		} else {
 			return color;
 		}
