180017ede6a9e030aba624e6b62f5e2d63125e57
BuildTools <rstikfort@gmail.com>
2017-06-08 20:47:57 +0200

    Improved variable cloud coverage even more. Still not quite happy with it.

--- shaders/lib/cloudCoverage.glsl
+++ shaders/lib/cloudCoverage.glsl
@@ -11,7 +11,7 @@ float noise1D(float p){
 
 float getCloudCoverage(){
     #if defined VOLUMETRIC_CLOUDS && defined VARIABLE_CLOUD_COVERAGE
-        return mix((1.0f - noise1D(frameTimeCounter * 0.005)) * 0.35f + 0.65f, 1.0f, rainStrength);
+        return mix((1.0f - noise1D(frameTimeCounter * 0.005)) * 0.3f + 0.7f, 1.0f, rainStrength);
     #else
         return 1.0f;
     #endif
