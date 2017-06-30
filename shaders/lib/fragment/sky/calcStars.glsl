acff021dd447be1de381fdc5ffad75b408c372cc
BuildTools <rstikfort@gmail.com>
2017-06-30 15:51:03 +0200

    Made the procedural galaxy look more realistic.

--- shaders/lib/fragment/sky/calcStars.glsl
+++ shaders/lib/fragment/sky/calcStars.glsl
@@ -16,7 +16,7 @@
 
 			float cosT = max(dot(fposition.rgb,upVec),0.0);
 
-			vec3 starCoord = wVec*(50.0/(wVec.y));
+			vec3 starCoord = wVec*(50.0 / wVec.y);
 			
 			//Curve the fragposition so that the stars and the galaxy doesnt look weird
 			starCoord *= mix(1.0, cosT, 1.0 - cosT);
@@ -30,25 +30,25 @@
 				
 				#ifdef DRAW_GALAXY
 				
-					float galaxyNoise = texture2D(noisetex, fract(starCoord / 40000.0).xz).x;
-					galaxyNoise += (texture2D(noisetex, fract(starCoord / 20000.0).xz).x * 1.5 - 0.5) * 0.5;
+					float galaxyNoise = length((starCoord.z + 50.0) * 0.01 + 0.25);
 					
-					float oldGalaxy = (texture2D(noisetex, fract(starCoord / 10000.0).xz).x * 1.5 - 1.5) * 0.25;
-						oldGalaxy += texture2D(noisetex, fract(starCoord / 5000.0).xz).x * 0.125;
-						oldGalaxy += texture2D(noisetex, fract(starCoord / 2500.0).xz).x * 0.125 * 0.5;
-						oldGalaxy += texture2D(noisetex, fract(starCoord / 1250.0).xz).x * 0.125 * 0.25;
+					float oldGalaxy = texture2D(noisetex, fract(starCoord / 5000.0).xz).x * 0.125;
+						  oldGalaxy += texture2D(noisetex, fract(starCoord / 2500.0).xz).x * 0.125 * 0.5;
+						  oldGalaxy += texture2D(noisetex, fract(starCoord / 1250.0).xz).x * 0.125 * 0.25;
 					
 					galaxyNoise += oldGalaxy;
 					galaxyNoise = min(galaxyNoise, 1.0 - galaxyNoise);
 					galaxyNoise = max(galaxyNoise * 8.0 - 1.5,0.0);
-					galaxyNoise *= 0.5;
+					galaxyNoise *= 0.45;
+					galaxyNoise *= galaxyNoise * 0.8;
 				
 					vec3 galaxy = vec3(galaxyNoise);
-						galaxy *= mix(normalize(vec3(0.8, 0.5, 1.0)), vec3(0.1, 0.5, 1.0) * 100.0, pow(oldGalaxy * 0.75 + 0.25, 3.0));
-						galaxy = mix(galaxy, vec3(1.0) * 50.0, pow(vec3(dot(galaxy * 0.14, vec3(0.33333))), vec3(7.0)));
-						galaxy *= time[1].y;
+						 galaxy *= mix(vec3(1.0, 1.0, 2.0), vec3(1.0, 0.75, 0.5) * 100.0, pow(oldGalaxy * 0.75 + 0.25, 3.0));
+						 galaxy = mix(galaxy, vec3(1.0) * 50.0, pow(vec3(dot(galaxy * 0.14, vec3(0.33333))), vec3(7.0)));
+						 galaxy += 1.0;
+						 galaxy *= time[1].y;
 
-				return mix(color, mix(vec3(3.0), vec3(galaxy * 3.0), transition_fading),(star * 2.0 * galaxy + galaxy * 0.01 * 0.5 * transition_fading) * cosT * time[1].y * (1.0 - rainStrength) * (1.0 - moondisk));
+				return mix(color, mix(vec3(1.0), galaxy, transition_fading),(star * 2.0 * galaxy + galaxy * 0.001 * transition_fading) * cosT * time[1].y * (1.0 - rainStrength) * (1.0 - moondisk));
 				
 			#else
 				return mix(color, vec3(3.0),star * 2.0 * cosT * time[1].y * (1.0 - rainStrength) * (1.0 - moondisk));
