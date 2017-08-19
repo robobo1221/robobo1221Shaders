af412f0d8d43d4ecd6f2d6501f05783cdc4b82ce
BuildTools <rstikfort@gmail.com>
2017-08-19 19:15:04 +0200

    Improved on water fog

--- shaders/lib/fragment/waterFog.glsl
+++ shaders/lib/fragment/waterFog.glsl
@@ -14,14 +14,18 @@
 
 	vec3 getWaterDepthFog(vec3 color, vec3 fragpos, vec3 fragpos2){
 
-		vec3 lightCol = mix(sunlight, pow(moonlight, vec3(0.4545)), time[1].y);
+		vec3 lightCol = sunlight;
 
 		float depth = getWaterDepth(fragpos, fragpos2); // Depth of the water volume
-		depth *= mix(iswater, 1.0, isEyeInWater);
+			  depth *= mix(iswater, 1.0, isEyeInWater);
 
 		float depthFog = 1.0 - clamp(exp2(-depth * DEPTH_FOG_DENSITY), 0.0, 1.0); // Beer's Law
 
-		float sunAngleCosine = pow(clamp(dot(normalize(fragpos.rgb), lightVector), 0.0, 1.0), 8.0);
+		float sunAngleCosine = 1.0 - clamp(dot(normalize(fragpos.rgb), lightVector), 0.0, 1.0);
+			  sunAngleCosine = sunAngleCosine*sunAngleCosine*(3.0 - 2.0 * sunAngleCosine);
+			  sunAngleCosine = 1.0 / sunAngleCosine - 1.0;
+			  sunAngleCosine /= sunAngleCosine * 0.02 + 1.0;
+
 		float NdotL = dot(compositeNormals, lightVector);
 		float SSS = pow(getWaterScattering(NdotL), 2.0);
 
@@ -29,10 +33,10 @@
 			 if (isEyeInWater < 0.9){
 				fogColor = (fogColor * (pow(aux2.b, skyLightAtten) + 0.25)) * 0.75;
 				fogColor = mix(fogColor, (fogColor * lightCol) * 3.75, SSS * (1.0 - rainStrength) * shadowsForward);
-				fogColor = mix(fogColor, (fogColor * lightCol) * 6.0,(sunAngleCosine * shadowsForward) * (transition_fading * (1.0 - pow(max(NdotL,0.0), 2.0))) * (1.0 - rainStrength));
+				fogColor = fogColor * (1.0 + lightCol * (sunAngleCosine * shadowsForward) * transition_fading * (1.0 - rainStrength));
 				}
-			 
-		color *= pow(vec3(0.1, 0.5, 0.8), vec3(depth) * 0.25);
+			 const vec3 waterCoeff = vec3(0.4510, 0.0867, 0.0476) / log(2.0);
+		color *= pow(vec3(0.1, 0.5, 0.8), vec3(depth) * 0.2);
 
 		return mix(color, fogColor, depthFog);
 	}
