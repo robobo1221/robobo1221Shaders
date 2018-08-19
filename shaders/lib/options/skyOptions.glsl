a5d045f64bef44f6f51c760ca7ec4b47ad5d25c3
robobo1221 <robobo1221@gmail.com>
2018-08-19 19:56:41 +0200

    gucci gang

--- shaders/lib/options/skyOptions.glsl
+++ shaders/lib/options/skyOptions.glsl
@@ -2,7 +2,7 @@
 #define moon_illuminance 0.32
 
 #define sunColorBase (blackbody(5778.0) * sun_illuminance)
-#define moonColorBase (blackbody(4000.0) * moon_illuminance)
+#define moonColorBase (blackbody(6000.0) * moon_illuminance)
 
 //Sky coefficients and heights
 
