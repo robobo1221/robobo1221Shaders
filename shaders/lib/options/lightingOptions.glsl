0e283aa070df8b4760c75884ba4a9d5bf2a095c7
robobo1221 <robobo1221@gmail.com>
2018-08-22 21:15:03 +0200

    Lowered the lumens of lightsources

--- shaders/lib/options/lightingOptions.glsl
+++ shaders/lib/options/lightingOptions.glsl
@@ -2,7 +2,7 @@ const int shadowMapResolution = 2048;
 const float rShadowMapResolution = 1.0 / float(shadowMapResolution);
 const float shadowDistance = 120.0;
 
-#define torchIlluminance 1600.0
+#define torchIlluminance 800.0
 #define torchColor (blackbody(2700.0) * torchIlluminance)
 
 #define COLOURED_SHADOWS
