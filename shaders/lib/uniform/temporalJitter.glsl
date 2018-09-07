f686e90e4a08644e3737d8206e9ffefcf8ef13e8
robobo1221 <robobo1221@gmail.com>
2018-09-07 19:13:40 +0200

    @Zombye#7365 Fix?

--- shaders/lib/uniform/temporalJitter.glsl
+++ shaders/lib/uniform/temporalJitter.glsl
@@ -1,21 +1,21 @@
-const vec2 bayerSequence4x4[16] = vec2[16](vec2(0, 3),
-                                           vec2(8, 11),
-                                           vec2(2, 1),
-                                           vec2(10, 9),
-                                           vec2(12, 15),
-                                           vec2(4, 7),
-                                           vec2(14, 13),
-                                           vec2(6, 5),
-                                           vec2(3, 0),
-                                           vec2(11, 8),
-                                           vec2(1, 2),
-                                           vec2(9, 10),
-                                           vec2(15, 12),
-                                           vec2(7, 4),
-                                           vec2(13, 14),
-                                           vec2(5, 6));
+const vec2 bayerSequence4x4[16] = vec2[16](vec2(0, -3) / 16.0,
+                                           vec2(-8, 11) / 16.0,
+                                           vec2(2, 1) / 16.0,
+                                           vec2(-10, -9) / 16.0,
+                                           vec2(12, -15) / 16.0,
+                                           vec2(-4, 7) / 16.0,
+                                           vec2(14, 13) / 16.0,
+                                           vec2(-6, -5) / 16.0,
+                                           vec2(-3, 0) / 16.0,
+                                           vec2(11, -8) / 16.0,
+                                           vec2(-1, -2) / 16.0,
+                                           vec2(9, 10) / 16.0,
+                                           vec2(-15, 12) / 16.0,
+                                           vec2(7, -4) / 16.0,
+                                           vec2(-13, -14) / 16.0,
+                                           vec2(5, 6) / 16.0);
 
 vec2 calculateTemporalJitter() {
     vec2 pixelSize = 2.0 / vec2(viewWidth, viewHeight);
-    return (bayerSequence4x4[int(mod(frameCounter, 16))] * (2.0 / 15.0) - 1.0) * pixelSize;
+    return (bayerSequence4x4[int(mod(frameCounter, 16))] * 2.0 - 1.0) * pixelSize;
 }
