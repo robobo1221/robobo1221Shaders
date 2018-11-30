8f94f5f0632e5d7809e3a93456a567ebf845c3e2
robobo1221 <rstikfort@gmail.com>
2018-11-30 20:41:10 +0100

    :b:etter

--- shaders/lib/fragment/directLighting.glsl
+++ shaders/lib/fragment/directLighting.glsl
@@ -101,7 +101,7 @@ float calculateTorchLightAttenuation(float lightmap){
 				float LoN = clamp01(dot(sampleVector, normal));
 				if (LoN <= 0.0) continue;
 
-				float falloff = 1.0 / (normFactor * rOffsetSize * 16384.0 + 0.25);
+				float falloff = 1.0 / (normFactor * rOffsetSize * 16384.0 + 0.5);
 				/*
 				float waterMask = texture2DLod(shadowcolor1, remappedCoord, 3).a * 2.0 - 1.0;
 
