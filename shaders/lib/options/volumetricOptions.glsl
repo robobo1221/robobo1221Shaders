4f8542ec6f486a3814bf26c1f98434b54752897f
robobo1221 <rstikfort@gmail.com>
2018-12-17 14:22:27 +0100

    Changed the default density of clouds to 0.05

--- shaders/lib/options/volumetricOptions.glsl
+++ shaders/lib/options/volumetricOptions.glsl
@@ -8,7 +8,7 @@
 #define VC_MULTISCAT_QUALITY 3  //[1 2 3 4 5 6]
 
 #define volumetric_cloudThicknessMult 1.0 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0]
-#define volumetric_cloudDensity 0.0125  //[0.005 0.075 0.01 0.0125 0.015 0.0175 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.06 0.07 0.08 0.09 0.1]
+#define volumetric_cloudDensity 0.05  //[0.005 0.075 0.01 0.0125 0.015 0.0175 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.06 0.07 0.08 0.09 0.1]
 #define volumetric_cloudHeight 1600.0   //[100.0 110.0 120.0 130.0 140.0 160.0 180.0 200.0 220.0 240.0 260.0 280.0 300.0 400.0 500.0 600.0 700.0 800.0 900.0 1000.0 1200.0 1400.0 1600.0 1800.0 2000.0]
 #define volumetric_cloudMinHeight volumetric_cloudHeight
 
