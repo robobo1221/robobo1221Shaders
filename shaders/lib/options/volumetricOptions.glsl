875912601cdedbc5a3944d8168ecb4c295b22fcf
robobo1221 <rstikfort@gmail.com>
2019-01-26 14:02:47 +0100

    Added more values to the VC sunlight quality. Also renamed quality in multiscattering to iterations

--- shaders/lib/options/volumetricOptions.glsl
+++ shaders/lib/options/volumetricOptions.glsl
@@ -1,11 +1,11 @@
 //#define VOLUMETRIC_CLOUDS
 #define VC_QUALITY 10 //[5 10 15 20 24 32 64 128 256 512]
-#define VC_SUNLIGHT_QUALITY 5 //[2 3 4 5 6 7 8 9 10 12 14 16 18 20]
+#define VC_SUNLIGHT_QUALITY 5 //[2 3 4 5 6 7 8 9 10 12 14 16 18 20 22 24 26 28 30 32 36 40 44 48 52 56 60 64]
 #define VC_NOISE_OCTAVES 4 //[3 4 5 6 7]
 #define VC_LOCAL_COVERAGE
 //#define VC_SHADOWS
-#define VC_MULTISCAT    //Simulates multiscattering. Very performance intensive!
-#define VC_MULTISCAT_QUALITY 3  //[1 2 3 4 5 6]
+#define VC_MULTISCAT    //Simulates multiscattering.
+#define VC_MULTISCAT_QUALITY 3  //[1 2 3 4 5 6 7 8]
 
 #define volumetric_cloudThicknessMult 1.0 //[0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0]
 #define volumetric_cloudDensity 0.05  //[0.005 0.075 0.01 0.0125 0.015 0.0175 0.02 0.025 0.03 0.035 0.04 0.045 0.05 0.06 0.07 0.08 0.09 0.1]
